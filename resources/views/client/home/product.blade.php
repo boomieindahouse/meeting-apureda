d15f84e17e5591c9af9639708a124723d7f2f5e2
boomieindahouse <prawitsanat@hotmail.com>
2025-03-04 17:08:20 +0700

    add img product in card product section

--- resources/views/client/home/product.blade.php
+++ resources/views/client/home/product.blade.php
@@ -1,28 +1,31 @@
 <section class="grid grid-cols-3">
     <!-- Card 1 -->
-    <div class="relative bg-gray-100 shadow-lg overflow-hidden h-[800px]">
-        <img src="{{ asset('images/homepage/product/productbg1.png') }}" class="w-full h-[800px] object-cover opacity-20">
+    <div class="relative bg-gray-100 shadow-lg overflow-hidden h-[900px]">
+        <img src="{{ asset('images/homepage/product/productbg1.png') }}" class="w-full h-[900px] object-cover opacity-20">
+        <img src="{{ asset('images/homepage/product/prod1.png') }}" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-[full] object-cover">
         <div class="absolute bottom-0 bg-gradient-to-t from-black to-transparent w-full p-10 text-white">
             <h3 class="text-2xl font-semibold">ปั๊มลมและอุปกรณ์</h3>
-            <a href="#" class="inline-block mt-2 text-white border border-white rounded-full px-4 py-2">→</a>
+            <a href="#" class="inline-block bg-primary mt-2 text-white rounded-full px-3 py-2 border border-transparent hover:border hover:border-white hover:bg-transparent duration-300">→</a>
         </div>
     </div>
 
     <!-- Card 2 -->
-    <div class="relative bg-gray-100 shadow-lg overflow-hidden h-[800px]">
-        <img src="{{ asset('images/homepage/product/productbg1.png') }}" class="w-full h-[800px] object-cover opacity-20">
+    <div class="relative bg-gray-100 shadow-lg overflow-hidden h-[900px]">
+        <img src="{{ asset('images/homepage/product/productbg1.png') }}" class="w-full h-[900px] object-cover opacity-20">
+        <img src="{{ asset('images/homepage/product/prod2.png') }}" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-[full] object-cover">
         <div class="absolute bottom-0 bg-gradient-to-t from-black to-transparent w-full p-10 text-white">
             <h3 class="text-2xl font-semibold">ไส้กรองและอะไหล่</h3>
-            <a href="#" class="inline-block mt-2 text-white border border-white rounded-full px-4 py-2">→</a>
+            <a href="#" class="inline-block bg-primary mt-2 text-white rounded-full px-3 py-2 border border-transparent hover:border hover:border-white hover:bg-transparent duration-300">→</a>
         </div>
     </div>
 
     <!-- Card 3 -->
-    <div class="relative bg-gray-100 shadow-lg overflow-hidden h-[800px]">
-        <img src="{{ asset('images/homepage/product/productbg1.png') }}" class="w-full h-[800px] object-cover opacity-20">
+    <div class="relative bg-gray-100 shadow-lg overflow-hidden h-[900px]">
+        <img src="{{ asset('images/homepage/product/productbg1.png') }}" class="w-full h-[900px] object-cover opacity-20">
+        <img src="{{ asset('images/homepage/product/prod3.png') }}" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-full object-cover">
         <div class="absolute bottom-0 bg-gradient-to-t from-black to-transparent w-full p-10 text-white">
             <h3 class="text-2xl font-semibold">ท่ออลูมิเนียม</h3>
-            <a href="#" class="inline-block mt-2 text-white border border-white rounded-full px-4 py-2">→</a>
+            <a href="#" class="inline-block bg-primary mt-2 text-white rounded-full px-3 py-2 border border-transparent hover:border hover:border-white hover:bg-transparent duration-300">→</a>
         </div>
     </div>
 
